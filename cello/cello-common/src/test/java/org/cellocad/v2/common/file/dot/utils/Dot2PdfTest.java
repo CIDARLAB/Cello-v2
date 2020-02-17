ff28a00cef7316cf36a5a48c4737e6b4bcce5bcc
Timothy Jones <jonests@bu.edu>
2020-02-17 16:35:17 -0500

    Move Dot2PdfTest.

--- cello/cello-common/src/test/java/org/cellocad/v2/common/file/dot/utils/Dot2PdfTest.java
+++ cello/cello-common/src/test/java/org/cellocad/v2/common/file/dot/utils/Dot2PdfTest.java
@@ -18,7 +18,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package org.cellocad.v2.common.test;
+package org.cellocad.v2.common.file.dot.utils;
 
 import java.io.File;
 import java.io.IOException;
